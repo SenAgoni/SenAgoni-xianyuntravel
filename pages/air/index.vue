695b20c97de07d788b77d9e90e0c696cc268db9e
SenAgoni <Agoni@740657284@163.com>
2019-10-15 10:02:37 +0800

    特价机票列表渲染

--- pages/air/index.vue
+++ pages/air/index.vue
@@ -38,7 +38,17 @@
 
     <!-- 特价机票 -->
     <div class="air-sale">
-        
+        <el-row type="flex" class="air-sale-pic" justify="space-between">
+            <el-col :span="6" v-for="(item, index) in sales" :key="index">
+                <nuxt-link :to="`/air/flights?departCity=${item.departCity}&departCode=${item.departCode}&destCity=${item.destCity}&destCode=${item.destCode}&departDate=${item.departDate}`">
+                    <img :src="item.cover"/>
+                    <el-row class="layer-bar" type="flex" justify="space-between">
+                        <span>{{item.departCity}}-{{item.destCity}}</span>
+                        <span>￥{{item.price}}</span>
+                    </el-row>
+                </nuxt-link>
+            </el-col>
+        </el-row>
     </div>
   </section>
 </template>
@@ -46,8 +56,20 @@
 <script>
 import Search from '@/components/air/search'
 export default {
+  data(){
+    return {
+      sales:[],
+    }
+  },
   components:{
     Search,
+  },
+  mounted(){
+    this.$axios({
+      url:"/airs/sale"
+    }).then(res=>{
+      this.sales = res.data.data;
+    })
   }
 }
 </script>
